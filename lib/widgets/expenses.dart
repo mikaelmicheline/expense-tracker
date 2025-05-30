b0220c27e7cde48786aa6e35188897e06ca6cb61
Mikael Micheline <105074940+mikaelmicheline@users.noreply.github.com>
2025-05-30 15:32:58 -0300

    refactor expenses and expenses list widget

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
-import 'package:expense_tracker/expenses_list.dart';
+import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -45,9 +46,25 @@ class _ExpensesState extends State<Expenses> {
         category: ExpenseCategory.health),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      context: context,
+      builder: (ctx) => NewExpense(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('ExpenseTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: Icon(Icons.add_sharp),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           Text('chart'),
