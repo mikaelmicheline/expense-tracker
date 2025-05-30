75eb984a2f19a3300d47fe3ceef14dbc382aea20
Mikael Micheline <105074940+mikaelmicheline@users.noreply.github.com>
2025-05-30 15:32:19 -0300

    change expense model

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
+
+final dateFormatter = DateFormat.yMd();
 
 enum ExpenseCategory {
   groceries,
@@ -11,6 +15,16 @@ enum ExpenseCategory {
   utilities,
   leisure,
 }
+
+const categoryIcons = {
+  ExpenseCategory.groceries: Icons.shopping_cart_sharp,
+  ExpenseCategory.health: Icons.medication_sharp,
+  ExpenseCategory.transportation: Icons.motorcycle_sharp,
+  ExpenseCategory.house: Icons.house_sharp,
+  ExpenseCategory.education: Icons.book_sharp,
+  ExpenseCategory.utilities: Icons.wifi_sharp,
+  ExpenseCategory.leisure: Icons.beach_access_sharp
+};
 
 class Expense {
   Expense(
@@ -25,4 +39,8 @@ class Expense {
   final double amount;
   final DateTime date;
   final ExpenseCategory category;
+
+  String get formattedDate {
+    return dateFormatter.format(date);
+  }
 }
